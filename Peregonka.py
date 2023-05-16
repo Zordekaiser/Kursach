3c5b19ebc63f6618493ae471b2421eb678e7179c
Zordekaiser <igorsavicki29@gmail.com>
2023-05-16 18:59:52 +0300

    Test

--- Peregonka.py
+++ Peregonka.py
@@ -9,97 +9,95 @@ systemCollection = current_db["systemCollection"]
 dataCollection = current_db["dataCollection"]
 smartcol = systemCollection.count_documents(({'key': 'smart_phone'}))
 smarts = systemCollection.find(({'key': 'smart_phone'}))
-buffer = smarts.next()
-rot = buffer["name"]
-rot += ".scs"
-file = open(rot, "w+")
 
-MYstring = ""
-MYstring += buffer["name"]
-MYstring += "<-concept_smartphone;"
-file.write(MYstring)
-MYstring = ""
-file.write("=>nrel_main_idtf:")
-MYstring += "["
-MYstring += buffer["name"]
-MYstring += "]"
-file.write(MYstring)
-MYstring = ""
-file.write("(* <- lang_en;; <- name_en;;*);")
-file.write("=>nrel_OS:")
-MYstring += "["
-MYstring += buffer["OS"]
-MYstring += "]"
-file.write(MYstring)
-MYstring = ""
-file.write("(* <- concept_OS;;*);")
-file.write("=>nrel_processor:")
-MYstring += "["
-MYstring += buffer['processor']
-MYstring += "]"
-file.write(MYstring)
-MYstring = ""
-file.write("(* <- concept_processor;;*);")
-file.write("=>nrel_matrix:")
-MYstring += "["
-MYstring += buffer['display']
-MYstring += "]"
-file.write(MYstring)
-MYstring = ""
-file.write("(* <- concept_matrix;;*);")
-MYstring += "=>nrel_RAM:"
-MYstring += "["
-MYstring += buffer["RAM"]
-MYstring += "]"
-file.write(MYstring)
-MYstring = ""
-file.write("(* <- concept_RAM;;<-exact_value;;*);")
-file.write("=>nrel_HDD:")
-MYstring += "["
-MYstring += buffer["HDD"]
-MYstring += "]"
-file.write(MYstring)
-MYstring = ""
-file.write("(* <- concept_HDD;;<-exact_value;;*);")
-file.write("=>nrel_main_camera:")
-MYstring += "["
-MYstring += buffer["main_camera"]
-MYstring += "]"
-file.write(MYstring)
-MYstring = ""
-file.write("(* <- concept_camera;;<-exact_value;;*);")
-file.write("=>nrel_front_camera:")
-MYstring += "["
-MYstring += buffer["front_camera"]
-MYstring += "]"
-file.write(MYstring)
-MYstring = ""
-file.write("(* <- concept_camera;;<-exact_value;;*);")
-file.write("=>nrel_display_size:")
-MYstring += "["
-MYstring += buffer["display_size"]
-MYstring += "]"
-file.write(MYstring)
-MYstring = ""
-file.write("(* <- concept_matrix;;<-exact_value;;*);")
-file.write("=>nrel_display_resolution:")
-MYstring += "["
-MYstring += buffer["display_resolution"]
-MYstring += "]"
-file.write(MYstring)
-MYstring = ""
-file.write("(* <- concept_matrix;;<-exact_value;;*);")
-file.write("=>nrel_battery:")
-MYstring += "["
-MYstring += buffer["battery"]
-MYstring += "]"
-file.write(MYstring)
-MYstring = ""
-file.write("(* <- concept_battery;;<-exact_value;;*);;")
+for i in range(0, systemCollection.count_documents({"key": "smart_phone"})):
+    buffer = smarts.next()
+    rot = "SCS/Smartphones/"
+    rot += buffer["name"]
+    rot += ".scs"
+    file = open(rot, "w+")
 
+    MYstring = ""
+    MYstring += buffer["name"]
+    MYstring += "<-concept_smartphone;\n"
+    file.write(MYstring)
+    MYstring = "    "
+    file.write("=>nrel_main_idtf:\n")
+    MYstring += "["
+    MYstring += buffer["name"]
+    MYstring += "]\n"
+    file.write(MYstring)
+    MYstring = "    "
+    file.write("    (* <- lang_en;; <- name_en;;*);\n")
+    file.write("=>nrel_OS:\n")
+    MYstring += "["
+    MYstring += buffer["OS"]
+    MYstring += "]\n"
+    file.write(MYstring)
+    MYstring = "    "
+    file.write("    (* <- concept_OS;;*);\n")
+    file.write("=>nrel_processor:\n")
+    MYstring += "["
+    MYstring += buffer['processor']
+    MYstring += "]\n"
+    file.write(MYstring)
+    MYstring = "    "
+    file.write("    (* <- concept_processor;;*);\n")
+    file.write("=>nrel_matrix:\n")
+    MYstring += "["
+    MYstring += buffer['display']
+    MYstring += "]\n"
+    file.write(MYstring)
+    MYstring = ""
+    file.write("    (* <- concept_matrix;;*);\n")
+    MYstring += "=>nrel_RAM:\n"
+    MYstring += "    ["
+    MYstring += buffer["RAM"]
+    MYstring += "]\n"
+    file.write(MYstring)
+    MYstring = "    "
+    file.write("    (* <- concept_RAM;;<-exact_value;;*);\n")
+    file.write("=>nrel_HDD:\n")
+    MYstring += "["
+    MYstring += buffer["HDD"]
+    MYstring += "]\n"
+    file.write(MYstring)
+    MYstring = "    "
+    file.write("    (* <- concept_HDD;;<-exact_value;;*);\n")
+    file.write("=>nrel_main_camera:\n")
+    MYstring += "["
+    MYstring += buffer["main_camera"]
+    MYstring += "]\n"
+    file.write(MYstring)
+    MYstring = "    "
+    file.write("    (* <- concept_camera;;<-exact_value;;*);\n")
+    file.write("=>nrel_front_camera:\n")
+    MYstring += "["
+    MYstring += buffer["front_camera"]
+    MYstring += "]\n"
+    file.write(MYstring)
+    MYstring = "    "
+    file.write("    (* <- concept_camera;;<-exact_value;;*);\n")
+    file.write("=>nrel_display_size:\n")
+    MYstring += "["
+    MYstring += buffer["display_size"]
+    MYstring += "]\n"
+    file.write(MYstring)
+    MYstring = "    "
+    file.write("    (* <- concept_matrix;;<-exact_value;;*);\n")
+    file.write("=>nrel_display_resolution:\n")
+    MYstring += "["
+    MYstring += buffer["display_resolution"]
+    MYstring += "]\n"
+    file.write(MYstring)
+    MYstring = "    "
+    file.write("    (* <- concept_matrix;;<-exact_value;;*);\n")
+    file.write("=>nrel_battery:\n")
+    MYstring += "["
+    MYstring += buffer["battery"]
+    MYstring += "]\n"
+    file.write(MYstring)
+    MYstring = "    "
+    file.write("    (* <- concept_battery;;<-exact_value;;*);;\n")
 
-
-
-file.close()
-#for i in range(0, systemCollection.count_documents({"key": "smart_phone"})):
-
+    file.close()
