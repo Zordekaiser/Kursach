725215c43bd200058422fd39a47c9e9664209a9b
kirill <crazy.com559@gmail.com>
2023-06-02 01:02:55 +0300

    Раздал стиля

--- Foo.py
+++ Foo.py
@@ -1,13 +1,13 @@
 from sc_client.client import connect, disconnect, template_search
 from sc_client.constants import sc_types
-from sc_client.models import ScTemplate, ScTemplateResult
+from sc_client.models import ScTemplate, ScTemplateResult, ScAddr
 from sc_kpm.utils import get_system_idtf, get_link_content_data, get_element_by_norole_relation
 from sc_kpm import ScKeynodes
 
+url = "ws://localhost:8090/ws_json"
+connect(url)
 
-def get_smartphones_idtf():
-    url = "ws://localhost:8090/ws_json"
-    connect(url)
+def get_smartphones_idtf(): # Раздача названий телефонов
 
     concept_addr = ScKeynodes['concept_smartphone']
 
@@ -15,31 +15,45 @@ def get_smartphones_idtf():
     my_template.triple(concept_addr, sc_types.EDGE_ACCESS_VAR_POS_PERM, sc_types.NODE_VAR >> 'quest_node')
 
     search_results = template_search(my_template)
-    result_addrs = []
+    result_idtfs = []
     for result in search_results:
         result_addr = result.get('quest_node')
-        result_addrs.append(result_addr)
+        result_idtf = get_system_idtf(result_addr)
+        result_idtfs.append(result_idtf)
 
-    disconnect()
-    return result_addrs
+    return result_idtfs
 
 
-def get_params_smartphone(_smartphone_name: str):
-    url = "ws://localhost:8090/ws_json"
-    connect(url)
+def get_params_smartphone(_smartphone_name: str):  # Лутает все параметры и собирает в словарь + name
+    list_params = ['OS', 'processor', 'matrix', 'RAM', 'HDD', 'main_camera', 'front_camera', 'display_size', 'display_resolution', 'battery']
     result_dict = {}
 
-    try:
-        smartphone_addr = ScKeynodes[_smartphone_name]
-    except:
-        return False
-    processor_relation_addr = ScKeynodes['nrel_processor']
-    proc = get_system_idtf(get_element_by_norole_relation(smartphone_addr, processor_relation_addr))
-    result_dict['processor'] = proc
+    for param in list_params:
+        result_dict[param] = get_param(_smartphone_name, 'nrel_' + param)
 
-    disconnect()
+    result_dict.setdefault('name', _smartphone_name)
     return result_dict
 
 
-name = input()
-print(get_params_smartphone(name))
+def get_param(target: str, param: str): # Возвращает значение одной характеристики/комплектующей
+
+    smartphone = ScKeynodes[target]
+    nrel_addr = ScKeynodes[param]
+
+    result = get_element_by_norole_relation(smartphone, nrel_addr)
+    result = get_main_indtf(result)
+    return result
+
+
+def get_main_indtf(target: ScAddr):
+    addr = ScKeynodes['nrel_main_idtf']
+    if target.is_valid():
+        my_template = ScTemplate()
+        my_template.triple_with_relation(target, sc_types.EDGE_D_COMMON_VAR, sc_types.LINK_VAR >> 'link', sc_types.EDGE_ACCESS_VAR_POS_PERM, addr)
+        search_results = template_search(my_template)
+
+        return get_link_content_data(search_results[0].get('link'))
+
+
+print(get_params_smartphone('Motorola_Edge_30_Ultra'))
+disconnect()
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from telebot import types
 import emoji
 import pymongo
 from sc_client.client import connect, disconnect, create_elements
-from sc_kpm.utils import get_system_idtf, get_element_by_norole_relation, get_link_content_data
+from sc_kpm.utils import get_element_by_norole_relation, get_link_content_data
 from sc_kpm import ScKeynodes
 
 url = "ws://localhost:8090/ws_json"
