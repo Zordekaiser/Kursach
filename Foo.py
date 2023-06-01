ef9641a0260cac9281ef0f54964f355186cebcb5
kirill <crazy.com559@gmail.com>
2023-06-01 20:12:48 +0300

    added function

--- Foo.py
+++ Foo.py
@@ -1,33 +1,45 @@
-from sc_client.client import connect, disconnect, create_elements
-from sc_client.constants import sc_types, exceptions
-from sc_kpm.utils import get_system_idtf, get_element_by_norole_relation, get_link_content_data, get_edge
-from sc_kpm.utils import  get_element_by_role_relation, delete_edges
+from sc_client.client import connect, disconnect, template_search
+from sc_client.constants import sc_types
+from sc_client.models import ScTemplate, ScTemplateResult
+from sc_kpm.utils import get_system_idtf, get_link_content_data, get_element_by_norole_relation
 from sc_kpm import ScKeynodes
-
-# get_system_idtf(адрес) Для узлов
-# get_link_content_data(адрес) Для ссылок
-
 
 
 def get_smartphones_idtf():
     url = "ws://localhost:8090/ws_json"
     connect(url)
 
-    addr = ScKeynodes["concept_smartphone"]
-    trg = ScKeynodes["nrel_inclusion"]
-    results = []
+    concept_addr = ScKeynodes['concept_smartphone']
 
-    while True:
-        result = get_element_by_role_relation(addr, trg)
-        if result.is_valid():
-            results.append(get_system_idtf(result))
-            ScKeynodes.delete(get_system_idtf(result))
-        else:
-            break
+    my_template = ScTemplate()
+    my_template.triple(concept_addr, sc_types.EDGE_ACCESS_VAR_POS_PERM, sc_types.NODE_VAR >> 'quest_node')
+
+    search_results = template_search(my_template)
+    result_addrs = []
+    for result in search_results:
+        result_addr = result.get('quest_node')
+        result_addrs.append(result_addr)
 
     disconnect()
-    return results
+    return result_addrs
 
 
-a = get_smartphones_idtf()
-print(a)
+def get_params_smartphone(_smartphone_name: str):
+    url = "ws://localhost:8090/ws_json"
+    connect(url)
+    result_dict = {}
+
+    try:
+        smartphone_addr = ScKeynodes[_smartphone_name]
+    finally:
+        return False
+    processor_relation_addr = ScKeynodes['nrel_processor']
+    proc = get_system_idtf(get_element_by_norole_relation(smartphone_addr, processor_relation_addr))
+    result_dict['processor'] = proc
+
+    disconnect()
+    return result_dict
+
+
+name = input()
+print(get_params_smartphone(name))
