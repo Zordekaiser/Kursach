b2475a79e948882de2321511ce92b60ebeb24f9b
kirill <crazy.com559@gmail.com>
2023-06-06 14:31:49 +0300

    Раздал стиля3

--- Foo.py
+++ Foo.py
@@ -10,55 +10,76 @@ connect(url)
 
 
 def get_definition(name):
-    addr = ScKeynodes[name]
+    sys_idtf = 'definition_of_' + name
+    addr1 = ScKeynodes[sys_idtf]
+    nrel_addr1 = ScKeynodes['nrel_sc_text_translation']
     my_template = ScTemplate()
-    my_template.triple(addr, sc_types.EDGE_D_COMMON_VAR, sc_types.LINK_VAR >> 'link')
-    search_res = template_search(my_template)
-    addr_res = search_res[0].get('link')
-    data_res = get_link_content_data(addr_res)
+    my_template.triple_with_relation(
+        sc_types.NODE_VAR >> 'q_node',
+        sc_types.EDGE_D_COMMON_VAR,
+        addr1,
+        sc_types.EDGE_ACCESS_VAR_POS_PERM,
+        nrel_addr1
+    )
+    search_result = template_search(my_template)
+    temp_node_addr = search_result[0].get('q_node')
+    my_template1 = ScTemplate()
+    my_template1.triple(temp_node_addr, sc_types.EDGE_ACCESS_VAR_POS_PERM, sc_types.LINK_VAR >> 'res')
+    search_result = template_search(my_template1)
+    link_addr = search_result[1].get('res')
+    return get_link_content_data(link_addr)
 
-    return data_res
 
+def get_smartphones_addrs(): # Раздача названий телефонов
 
-def get_smartphones_idtf(): # Раздача названий телефонов
-
-    concept_addr = ScKeynodes['concept_smartphone']
+    concept_smartphone_addrs = ScKeynodes['concept_smartphone']
 
     my_template = ScTemplate()
-    my_template.triple(concept_addr, sc_types.EDGE_ACCESS_VAR_POS_PERM, sc_types.NODE_VAR >> 'quest_node')
+    my_template.triple(concept_smartphone_addrs, sc_types.EDGE_ACCESS_VAR_POS_PERM, sc_types.NODE_VAR >> 'q_node')
+    search_result = template_search(my_template)
+    result = []
+    for item in search_result:
+        result.append(item.get('q_node'))
 
-    search_results = template_search(my_template)
-    result_idtfs = []
-    for result in search_results:
-        result_addr = result.get('quest_node')
-        result_idtf = get_system_idtf(result_addr)
-        result_idtfs.append(result_idtf)
-
-    return result_idtfs
+    return result
 
 
-def get_params_smartphone(_smartphone_name: str):  # Лутает все параметры и собирает в словарь + name
-    list_params = ['OS', 'processor', 'matrix', 'RAM', 'HDD', 'main_camera', 'front_camera', 'display_size', 'display_resolution', 'battery']
+def get_processors_addrs(): # Раздача названий телефонов
+
+    concept_smartphone_addrs = ScKeynodes['concept_processor']
+
+    my_template = ScTemplate()
+    my_template.triple(concept_smartphone_addrs, sc_types.EDGE_ACCESS_VAR_POS_PERM, sc_types.NODE_VAR >> 'q_node')
+    search_result = template_search(my_template)
+    result = []
+    for item in search_result:
+        result.append(item.get('q_node'))
+
+    return result
+
+
+def get_params_smartphone(_smartphone_addr: ScAddr):  # Лутает все параметры и собирает в словарь + name
+    list_params = ['OS', 'processor', 'RAM', 'HDD', 'main_camera', 'front_camera', 'display_size', 'battery']
     result_dict = {}
 
     for param in list_params:
-        result_dict[param] = get_param(_smartphone_name, 'nrel_' + param)
+        result_dict[param] = get_param(_smartphone_addr, 'nrel_' + param)
 
-    result_dict.setdefault('name', _smartphone_name)
+    result_dict.setdefault('name', get_system_idtf(_smartphone_addr))
     return result_dict
 
-def get_params_processor(_smartphone_name: str):  # Лутает все параметры и собирает в словарь + name
-    list_params = ['number_of_cores', 'frequency', 'graphics', 'manufacturer']
+def get_params_processor(_processor_addr: ScAddr):  # Лутает все параметры и собирает в словарь + name
+    list_params = ['count_of_cores', 'frenquency', 'processor_manufacturer']
     result_dict = {}
 
     for param in list_params:
-        result_dict[param] = get_param(_smartphone_name, 'nrel_' + param)
+        result_dict[param] = get_param(_processor_addr, 'nrel_' + param)
 
-    result_dict.setdefault('name', _smartphone_name)
+    result_dict.setdefault('name', get_system_idtf(_processor_addr))
     return result_dict
 
 def get_params_app(_smartphone_name: str):  # Лутает все параметры и собирает в словарь + name
-    list_params = ['name', 'definition']
+    list_params = ['definition']
     result_dict = {}
 
     for param in list_params:
@@ -67,14 +88,25 @@ def get_params_app(_smartphone_name: str):  # Лутает все парамет
     result_dict.setdefault('name', _smartphone_name)
     return result_dict
 
-def get_param(target: str, param: str): # Возвращает значение одной характеристики/комплектующей
+def get_param(target: ScAddr, param: str): # Возвращает значение одной характеристики/комплектующей
 
-    smartphone = ScKeynodes[target]
     nrel_addr = ScKeynodes[param]
 
-    result = get_element_by_norole_relation(smartphone, nrel_addr)
-    result = get_main_indtf(result)
-    return result
+    my_template = ScTemplate()
+    my_template.triple_with_relation(
+        sc_types.NODE_VAR >> 'q_node',
+        sc_types.EDGE_D_COMMON_VAR,
+        target,
+        sc_types.EDGE_ACCESS_VAR_POS_PERM,
+        nrel_addr
+    )
+    search = template_search(my_template)
+    if search:
+        result = search[0].get('q_node')
+        result = get_system_idtf(result)
+        return result
+    else:
+        return None
 
 
 def get_main_indtf(target: ScAddr):
@@ -85,6 +117,8 @@ def get_main_indtf(target: ScAddr):
         search_results = template_search(my_template)
 
         return get_link_content_data(search_results[0].get('link'))
+
+
 
 
 disconnect()
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pymongo
 from sc_client.client import connect, disconnect, create_elements
 from sc_kpm.utils import get_element_by_norole_relation, get_link_content_data
 from sc_kpm import ScKeynodes
-from Foo import get_smartphones_idtf, get_params_smartphone, get_params_app, get_definition
+from Foo import get_smartphones_addrs, get_processors_addrs, get_params_smartphone, get_params_app, get_params_processor, get_definition
 from operator import itemgetter
 
 url = "ws://localhost:8090/ws_json"
@@ -13,23 +13,23 @@ connect(url)
 
 def get_processor():
     buffer = []
-    idtf = get_smartphones_idtf('concept_processor')
+    idtf = get_processors_addrs()[1:]
     for i in idtf:
-        buffer.append(get_params_smartphone(i))
+        buffer.append(get_params_processor(i))
     return buffer
 def get_smartphones():
     buffer = []
-    idtf = get_smartphones_idtf('concept_smartphone')
+    idtf = get_smartphones_addrs()[3:]
     for i in idtf:
         buffer.append(get_params_smartphone(i))
     return buffer
 
-def get_apps():
-    buffer = []
-    idtf = get_smartphones_idtf('concept_application')
-    for i in idtf:
-        buffer.append(get_params_app(i))
-    return buffer
+# def get_apps():
+#     buffer = []
+#     idtf = get_smartphones_idtf('concept_application')
+#     for i in idtf:
+#         buffer.append(get_params_app(i))
+#     return buffer
 
 def extract_arg(arg):
     buffer = arg.split()
@@ -91,9 +91,9 @@ def create(message):
     else:
         bot.send_message(message.chat.id, emoji.emojize("<i>Успешно создан! :thumbs_up:</i>"), parse_mode='html')
 
-
+# print(get_smartphones())
 class Smarphones:
-    list_params = ['name', 'OS', 'processor', 'matrix', 'RAM', 'HDD', 'main_camera', 'front_camera', 'display_size', 'display_resolution', 'battery']
+    list_params = ['name', 'OS', 'processor', 'RAM', 'HDD', 'main_camera', 'front_camera', 'display_size', 'battery']
     buffer_smartphones = []
     buffer_smartphones_counter = 0
 
@@ -103,7 +103,7 @@ class Apps:
     buffer_apps_counter = 0
 
 class Processors:
-    list_params = ['name', 'number_of_cores', 'frequency', 'graphics', 'manufacturer']
+    list_params = ['name', 'count_of_cores', 'frenquency', 'processor_manufacturer']
     buffer_processors = []
     buffer_processors_counter = 0
 
@@ -152,7 +152,7 @@ def func(message):
     elif message.text == 'Хочу смотреть фильмы и сериалы':
         mess = ""
 
-        buf_smartphone.buffer_smartphones = sorted(get_smartphones(), key=itemgetter('display_resolution', 'display_size'))
+        buf_smartphone.buffer_smartphones = sorted(get_smartphones(), key=itemgetter('display_size'))
         for i in buf_smartphone.list_params:
             mess += f"<i>{i}: {buf_smartphone.buffer_smartphones[buf_smartphone.buffer_smartphones_counter][i]}\n</i>"
         mess += "<i>Выберите дейтвие:</i>"
@@ -235,15 +235,13 @@ def func(message):
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         button1 = types.KeyboardButton('Процессор')
         button2 = types.KeyboardButton('Камера')
-        button3 = types.KeyboardButton('Матрицы и их разновидности')
-        button4 = types.KeyboardButton('Сенсоры и их разновидности')
+        button3 = types.KeyboardButton('Матрицы')
         button5 = types.KeyboardButton('Операционные системы и их разновидности')
-        button6 = types.KeyboardButton('Приложения')
         button = types.KeyboardButton('Вернуться в главное меню')
-        markup.add(button1, button2, button3, button4, button5, button6, button)
+        markup.add(button1, button2, button3, button5, button)
         bot.send_message(message.chat.id, mess, parse_mode='html', reply_markup=markup)
     elif message.text == 'Процессор':
-        text = get_definition('concept_processor')
+        text = get_definition('processor')
         mess = f'<i>{text}</i>'
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         button1 = types.KeyboardButton('Какие процессоры сущесвтуют?')
@@ -266,22 +264,22 @@ def func(message):
         bot.send_message(message.chat.id, mess, parse_mode='html', reply_markup=markup)
     elif message.text == 'Следующий процессор':
 
-        if buf_processor.buffer_processors_counter > len(buf_processor.buffer_processors):
-            buf_processor.buffer_processors_counter -= 1
+        if buf_processor.buffer_processors_counter < len(buf_processor.buffer_processors):
+            buf_processor.buffer_processors_counter += 1
             mess = ""
             for i in buf_processor.list_params:
                 mess += f"<i>{i}: {buf_processor.buffer_processors[buf_processor.buffer_processors_counter][i]}\n</i>"
             mess += "<i>Выберите дейтвие:</i>"
             markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            button1 = types.KeyboardButton('Следующее приложние')
-            button2 = types.KeyboardButton('Предыдущее приложение')
+            button1 = types.KeyboardButton('Следующий процессор')
+            button2 = types.KeyboardButton('Предыдущий процессор')
             button = types.KeyboardButton('Вернуться в главное меню')
             markup.add(button1, button2, button)
             bot.send_message(message.chat.id, mess, parse_mode='html', reply_markup=markup)
         else:
-            mess = "<i>Вы вернулись в начало списка</i>"
+            mess = "<i>Вы пришли в конец списка</i>"
             markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            button1 = types.KeyboardButton('Следующее приложние')
+            button1 = types.KeyboardButton('Предыдущий процессор')
             button = types.KeyboardButton('Вернуться в главное меню')
             markup.add(button1, button)
             bot.send_message(message.chat.id, mess, parse_mode='html', reply_markup=markup)
@@ -294,122 +292,45 @@ def func(message):
                 mess += f"<i>{i}: {buf_processor.buffer_processors[buf_processor.buffer_processors_counter][i]}\n</i>"
             mess += "<i>Выберите дейтвие:</i>"
             markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            button1 = types.KeyboardButton('Следующее приложние')
-            button2 = types.KeyboardButton('Предыдущее приложение')
+            button1 = types.KeyboardButton('Следующий процессор')
+            button2 = types.KeyboardButton('Предыдущий процессор')
             button = types.KeyboardButton('Вернуться в главное меню')
             markup.add(button1, button2, button)
             bot.send_message(message.chat.id, mess, parse_mode='html', reply_markup=markup)
         else:
             mess = "<i>Вы вернулись в начало списка</i>"
             markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            button1 = types.KeyboardButton('Следующее приложние')
+            button1 = types.KeyboardButton('Следующий процессор')
             button = types.KeyboardButton('Вернуться в главное меню')
             markup.add(button1, button)
             bot.send_message(message.chat.id, mess, parse_mode='html', reply_markup=markup)
     elif message.text == 'Камера':
-        text = get_definition('concept_camera')
+        text = get_definition('main_camera')
         mess = f'<i>{text}</i>'
-        text = buffer["definition"]
-        mess = f'<i>{underscoreToSpace(text)}</i>'
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         button1 = types.KeyboardButton('Вернуться к комплектующим')
         button2 = types.KeyboardButton('Вернуться в главное меню')
         markup.add(button1, button2)
         bot.send_message(message.chat.id, mess, parse_mode='html', reply_markup=markup)
-    elif message.text == 'Сенсоры и их разновидности' or message.text == 'Вернуться к сенсорам':
-        text = get_definition('concept_sensor')
-        mess = f'<i>{text}</i>'
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        button1 = types.KeyboardButton('Вернуться к комплектующим')
-        button2 = types.KeyboardButton('Акселерометр')
-        button3 = types.KeyboardButton('Гироскоп')
-        button4 = types.KeyboardButton('Датчик освещённости')
-        button5 = types.KeyboardButton('Датчик приближения')
-        button = types.KeyboardButton('Вернуться в главное меню')
-        markup.add(button1, button2, button3, button4, button5, button)
-        bot.send_message(message.chat.id, mess, parse_mode='html')
-        mess = "<i>Выберите команду:</i>"
-        bot.send_message(message.chat.id, mess, parse_mode='html', reply_markup=markup)
-    elif message.text == 'Акселерометр':
-        text = get_definition('concept_processor')
-        mess = f'<i>{text}</i>'
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        button1 = types.KeyboardButton('Вернуться к комплектующим')
-        button2 = types.KeyboardButton('Вернуться к сенсорам')
-        button = types.KeyboardButton('Вернуться в главное меню')
-        markup.add(button1, button2, button)
-        bot.send_message(message.chat.id, mess, parse_mode='html', reply_markup=markup)
-    elif message.text == 'Гироскоп':
-        text = get_definition('concept_processor')
-        mess = f'<i>{text}</i>'
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        button1 = types.KeyboardButton('Вернуться к комплектующим')
-        button2 = types.KeyboardButton('Вернуться к сенсорам')
-        button = types.KeyboardButton('Вернуться в главное меню')
-        markup.add(button1, button2, button)
-        bot.send_message(message.chat.id, mess, parse_mode='html', reply_markup=markup)
-    elif message.text == 'Датчик освещённости':
-        text = get_definition('concept_processor')
-        mess = f'<i>{text}</i>'
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        button1 = types.KeyboardButton('Вернуться к комплектующим')
-        button2 = types.KeyboardButton('Вернуться к сенсорам')
-        button = types.KeyboardButton('Вернуться в главное меню')
-        markup.add(button1, button2, button)
-        bot.send_message(message.chat.id, mess, parse_mode='html', reply_markup=markup)
-    elif message.text == 'Датчик приближения':
-        text = get_definition('concept_processor')
-        mess = f'<i>{text}</i>'
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        button1 = types.KeyboardButton('Вернуться к комплектующим')
-        button2 = types.KeyboardButton('Вернуться к сенсорам')
-        button = types.KeyboardButton('Вернуться в главное меню')
-        markup.add(button1, button2, button)
-        bot.send_message(message.chat.id, mess, parse_mode='html', reply_markup=markup)
-    elif message.text == 'Матрицы и их разновидности':
-        text = get_definition('concept_processor')
+    elif message.text == 'Матрицы':
+        text = get_definition('matrix')
         mess = f'<i>{text}</i>'
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         button1 = types.KeyboardButton('Вернуться к комплектующим')
         button2 = types.KeyboardButton('Вернуться в главное меню')
         markup.add(button1, button2)
         bot.send_message(message.chat.id, mess, parse_mode='html')
-        countOfMatrix = dataCollection.count_documents({'key': 'matrix'})
-        matrixs = dataCollection.find({'key': 'matrix'})
-        mess = 'Существуют следующие типы матриц '
-        text = ''
-        for i in range(0, countOfMatrix):
-            buffer = matrixs.next()
-            text += buffer["matrix"]
-            if i != countOfMatrix - 1:
-                text += ", "
-            else:
-                text += '.'
-        bot.send_message(message.chat.id, mess+text, parse_mode='html', reply_markup=markup)
-    elif message.text == 'Операционные системы и их разновидности':
-        text = get_definition('concept_OS')
-        mess = f'<i>{text}</i>'
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        button1 = types.KeyboardButton('Вернуться к комплектующим')
-        button2 = types.KeyboardButton('Вернуться в главное меню')
-        markup.add(button1, button2)
-        mess += '/nСуществуют следующие типы операционных систем: Android, iOS.'
-        bot.send_message(message.chat.id, mess+text, parse_mode='html', reply_markup=markup)
-    elif message.text == 'Приложения':
-        text = get_definition('concept_application')
-        mess = f'<i>{text}</i>'
-        mess += "/n"
 
-        buf_app.buffer_apps = get_apps()
-        for i in buf_app.list_params:
-            mess += f"<i>{i}: {buf_app.buffer_apps[buf_app.buffer_apps_counter][i]}\n</i>"
-        mess += "<i>Выберите дейтвие:</i>"
+    elif message.text == 'Операционные системы и их разновидности':
+        text = get_definition('OS')
+        mess = f'<i>{text}</i>'
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        button1 = types.KeyboardButton('Следующее приложение')
-        button2 = types.KeyboardButton('Предыдущее приложение')
-        button = types.KeyboardButton('Вернуться в главное меню')
-        markup.add(button1, button2, button)
+        button1 = types.KeyboardButton('Вернуться к комплектующим')
+        button2 = types.KeyboardButton('Вернуться в главное меню')
+        markup.add(button1, button2)
+        mess += '\n<i>Существуют следующие типы операционных систем: Android, iOS.</i>'
         bot.send_message(message.chat.id, mess, parse_mode='html', reply_markup=markup)
+
     elif message.text == 'Следующее приложение':
 
         if buf_app.buffer_apps_counter < len(buf_app.buffer_apps):
@@ -419,15 +340,15 @@ def func(message):
                 mess += f"<i>{i}: {buf_app.buffer_apps[buf_app.buffer_apps_counter][i]}\n</i>"
             mess += "<i>Выберите дейтвие:</i>"
             markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            button1 = types.KeyboardButton('Следующий телефон')
-            button2 = types.KeyboardButton('Предыдущий телефон')
+            button1 = types.KeyboardButton('Следующее приложение')
+            button2 = types.KeyboardButton('Предыдущее приложение')
             button = types.KeyboardButton('Вернуться в главное меню')
             markup.add(button1, button2, button)
             bot.send_message(message.chat.id, mess, parse_mode='html', reply_markup=markup)
         else:
             mess = "<i>Были представлены все приложения</i>"
             markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            button1 = types.KeyboardButton('Следующий телефон')
+            button1 = types.KeyboardButton('Предыдущее приложение')
             button = types.KeyboardButton('Вернуться в главное меню')
             markup.add(button1, button)
             bot.send_message(message.chat.id, mess, parse_mode='html', reply_markup=markup)
